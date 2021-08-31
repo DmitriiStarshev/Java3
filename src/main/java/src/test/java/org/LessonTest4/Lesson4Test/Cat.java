747be3504cf471994de6c3f38dbf988d0a26d6e4
Дмитрий Старшев <dstarshev@gmail.com>
2021-09-01 00:14:59 +0300

    Прошла 5я лекция

---
+++ src/main/java/src/test/java/org/LessonTest4/Lesson4Test/Cat.java
@@ -0,0 +1,35 @@
+package src.test.java.org.LessonTest4.Lesson4Test;
+
+public class Cat {
+    private String name;
+    private Integer age;
+
+    public Cat(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
